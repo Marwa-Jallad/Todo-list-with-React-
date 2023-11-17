c7e823a46b6b065a6ed2e61a7a20bdfdb415b056
Marwa-Jallad <marwa.jallad89@gmail.com>
2023-11-17 15:36:10 +0200

    Todo_list_React

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react"
 import "./styles.css"
+//import crypto from "crypto"; crypto is library to generate id but it didnt work in with me 
+import { v4 as uuidv4 } from 'uuid'; //
 
 export default  function App() { 
   const [newItem , setNewItem ]  = useState("")
-
+  const [Todos , setTodos] = useState([])
+  function handleSubmit(e){
+    e.preventDefault()
+  // setTodos([...Todos, {id: uuidv4(), title: newItem, completed: false}]) this way works but its better to use a new variable called (currentTodo) so we dont always start from (Todos) variable which is empty array 
+  setTodos(currentTodos => {
+  return [
+    ...currentTodos, {id: uuidv4(), title: newItem, completed: false}]})
+}
+console.log(Todos);
   return (<>
-  <form className="new-item-form">
+  <form  onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New item </label>
       <input value={newItem} onChange={e=>setNewItem(e.target.value)} type="text" id = "item"/>
